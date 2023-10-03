c795d94354b68a8fcdabf42215ee514ca49b87e7
nfaraz <faraznicolas.imei@gmail.com>
2023-10-02 21:36:51 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/faraznicolas/godesde0/variables"
 )
 
 func main() {
 	variables.MuestroEnteros()
+	variables.RestoVariables()
+	var estado, texto = variables.ConviertoaTexto(10520)
+	fmt.Println("estado", estado)
+	fmt.Println("texto", texto)
 }
