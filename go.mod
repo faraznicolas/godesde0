425f857c9309cf8f3b109ad66ebcf9cc671f1270
nfaraz <faraznicolas.imei@gmail.com>
2023-10-02 21:11:03 -0300

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/faraznicolas/godesde0
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/faraznicolas/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
